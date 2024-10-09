b8d6cda487acafa7d7dfa7a0c0106b4b028198ec
Arvee <arveedurante220@gmail.com>
2024-10-09 12:15:16 +0800

    simple fetching data

--- fetching-data/src/App.tsx
+++ fetching-data/src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+interface Post {
+  id: number,
+  title: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/posts`)
+        if (!response.ok) throw new Error('failed to fetch data')
+        const posts = await response.json()
+
+        setPosts(posts)
+        
+      } catch (error: any) {
+        console.log(error.message)
+      }      
+    }
+
+    fetchData()
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="tutorial">
+      <h1>Fetching Data</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
