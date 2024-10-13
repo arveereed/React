bb589b3666546f18b2b0d4ca1f52678235733b09
Arvee <arveedurante220@gmail.com>
2024-10-13 11:20:56 +0800

    create add new post function and use useMutation hook

--- react-query-2/src/App.tsx
+++ react-query-2/src/App.tsx
@@ -1,10 +1,11 @@
 import { useQuery, useMutation } from "@tanstack/react-query"
-import { getPosts } from "./api/posts"
+import { getPosts, addPost } from "./api/posts"
 
 interface Post {
   id: number,
   title: string
 }
+
 
 function App() {
   const { data: posts, isLoading, isError, error } = useQuery<Post[]>({
@@ -12,9 +13,16 @@ function App() {
     queryFn: getPosts
   })
 
+  const { mutate } = useMutation({
+    mutationFn: (title: string) => addPost(title)
+  })
+
   return (
     <>
     <h1>Posts List</h1>
+    <p>
+      <button onClick={() => mutate('New Post')}>Add new Post</button>
+    </p>
     {isLoading && (
       <div>Loading...</div>
     )}
--- react-query-2/src/api/posts.ts
+++ react-query-2/src/api/posts.ts
@@ -4,6 +4,15 @@ function wait(duration: number) {
   return new Promise(resolve => setTimeout(resolve, duration))
 }
 
-export const getPosts = () => {
+export const getPosts = async () => {
   return wait(1000).then(() => [...posts])
+}
+
+const generateId = () => {
+  return posts.length
+          ? posts.length + 1 : 1
+}
+
+export const addPost = async (title: string) => {
+  return wait(1000).then(() => posts.push({ id: generateId(), title }))
 }
